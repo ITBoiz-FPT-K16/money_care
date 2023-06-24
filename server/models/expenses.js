cbbdd6e46bc104eb444ce37f8d7c636da7ccbf50
husone <sonhvnde160056@fpt.edu.vn>
2023-06-24 20:21:08 +0700

    schema

---
+++ server/models/expenses.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;
+
+var Expense = new Schema({
+    amount: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    }
+});
+
+
+module.exports = {
+    Expenses : mongoose.model('Expense', Expense),
+    ExpenseSchema : Expense
+}
