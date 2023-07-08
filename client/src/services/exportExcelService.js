1d0af75086d646a6ccc808bae360789234222b4e
HoangNghia <hoangnghianguyendac@gmail.com>
2023-07-08 17:43:32 +0700

    update export excel

--- client/src/services/exportExcelService.js
+++ client/src/services/exportExcelService.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 export const exportExcel = async (time, token) => {
+    console.log("time>>>   ", time);
+
     try {
+        let timeExport;
+        if (time == "") {
+            timeExport = "all";
+        } else {
+            timeExport = time;
+        }
+
         let res = await axios.get(`${API_URL}/export/${time}`, {
             responseType: "blob",
             headers: { Authorization: `Bearer ${token}` },
@@ -12,7 +21,7 @@ export const exportExcel = async (time, token) => {
         const link = document.createElement("a");
 
         link.href = url;
-        link.setAttribute("download", `export_${time}.xlsx`);
+        link.setAttribute("download", `export_${timeExport}.xlsx`);
         document.body.appendChild(link);
         link.click();
     } catch (error) {
