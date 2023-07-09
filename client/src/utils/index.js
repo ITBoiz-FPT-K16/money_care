1e7662c2e43a5823b566c6fc9051677eb81fcbfc
giabao <baotran2115@gmail.com>
2023-07-09 16:24:33 +0700

    Report Component

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,7 +1,9 @@
 export const currencyFormat = (number) => {
-    return new Intl.NumberFormat("en-US", {
-        style: "decimal",
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    }).format(number);
+  return new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+    .format(number)
+    .replace(/(\.|,)00$/g, "");
 };
