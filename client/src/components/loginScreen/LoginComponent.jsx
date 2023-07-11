d88dc64c343d11ce6eb74056ebaa4fb57581d9d3
HoangNghia <hoangnghianguyendac@gmail.com>
2023-07-11 09:46:15 +0700

    fix cover img login

--- client/src/components/loginScreen/LoginComponent.jsx
+++ client/src/components/loginScreen/LoginComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import firebase from "firebase/compat/app";
 import "firebaseui/dist/firebaseui.css";
-
+import "./login.scss";
 import { useEffect } from "react";
 import StyledFirebaseAuth from "./StyledFirebaseAuth";
 
@@ -17,130 +17,137 @@ import coverPhoto from "../../assets/images/cover-photo.jpg";
 import moment from "moment";
 import { getTotalAmount } from "../../services/authService";
 const uiConfig = {
-  signInFlow: "popup",
-  signInSuccessUrl: "/home",
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
-  callbacks: {
-    signInSuccessWithAuthResult: () => false,
-  },
+    signInFlow: "popup",
+    signInSuccessUrl: "/home",
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+        signInSuccessWithAuthResult: () => false,
+    },
 };
 
 const LoginComponent = () => {
-  const currentUser = useSelector((state) => state.auth.auth.user);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [isAuth, setIsAuth] = React.useState(false);
-  console.log("isAuth", isAuth);
-  console.log("auth", auth);
+    const currentUser = useSelector((state) => state.auth.auth.user);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [isAuth, setIsAuth] = React.useState(false);
+    console.log("isAuth", isAuth);
+    console.log("auth", auth);
 
-  const doAfterAuth = async () => {
-    if (isAuth) {
-      console.log("doAfterAuth: isAuth is true");
-      const token = await auth.currentUser.getIdToken(true);
-      console.log("token from getIdToken()", token);
+    const doAfterAuth = async () => {
+        if (isAuth) {
+            console.log("doAfterAuth: isAuth is true");
+            const token = await auth.currentUser.getIdToken(true);
+            console.log("token from getIdToken()", token);
 
-      dispatch(
-        actionAuth.loginSuccess({
-          uid: auth.currentUser.uid,
-          name: auth.currentUser.displayName,
-          email: auth.currentUser.email,
-          accessToken: token,
-          refresh_token: auth.currentUser.refreshToken,
-          userImg: auth.currentUser.photoURL,
-        })
-      );
+            dispatch(
+                actionAuth.loginSuccess({
+                    uid: auth.currentUser.uid,
+                    name: auth.currentUser.displayName,
+                    email: auth.currentUser.email,
+                    accessToken: token,
+                    refresh_token: auth.currentUser.refreshToken,
+                    userImg: auth.currentUser.photoURL,
+                })
+            );
 
-      const totalAmount = await getTotalAmount(token);
-      if (totalAmount.errCode === 0) {
-        dispatch(actionAuth.setTotalAmount(totalAmount.data.total));
-      }
+            const totalAmount = await getTotalAmount(token);
+            if (totalAmount.errCode === 0) {
+                dispatch(actionAuth.setTotalAmount(totalAmount.data.total));
+            }
 
-      navigate("/home/transactions");
+            navigate("/home/transactions");
 
-      toast.success("Login Success");
-    }
-  };
+            toast.success("Login Success");
+        }
+    };
 
-  useEffect(() => {
-    const unregisterAuthObserver = auth.onAuthStateChanged((user) => {
-      setIsAuth(!!user);
+    useEffect(() => {
+        const unregisterAuthObserver = auth.onAuthStateChanged((user) => {
+            setIsAuth(!!user);
+        });
+        return () => unregisterAuthObserver();
     });
-    return () => unregisterAuthObserver();
-  });
 
-  useEffect(() => {
-    if (isAuth) {
-      doAfterAuth();
-    }
-  }, [isAuth]);
+    useEffect(() => {
+        if (isAuth) {
+            doAfterAuth();
+        }
+    }, [isAuth]);
 
-  return (
-    <>
-      {!isAuth ? (
-        <div className=" flex justify-center items-center">
-          <div
-            className="login rounded-lg"
-            style={{ height: "600px", width: "1000px" }}
-          >
-            <div className=" grid grid-cols-5" style={{ height: "100%" }}>
-              <div
-                className="col-span-3 rounded-tl-lg rounded-bl-lg h-100% "
-                style={{
-                  // setting background image
-                  backgroundImage: `url(${coverPhoto}) `,
-                }}
-              ></div>
-              <div
-                className="col-span-2 rounded-tr-lg rounded-br-lg h-100% text-center"
-                style={{
-                  backgroundColor: "#FBFFDC",
-                }}
-              >
-                <div className="p-5">
-                  <h3
-                    className="text-center my-3 title"
-                    style={{ color: "#F24C3D" }}
-                  >
-                    Login Page
-                  </h3>
-                  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-                  <p>
-                    You care about your money?
-                    <br />
-                    We care about you
-                  </p>
+    return (
+        <>
+            {!isAuth ? (
+                <div className="h-100vh flex items-center justify-center">
+                    <div
+                        className="login rounded-lg"
+                        style={{ height: "600px", width: "1000px" }}
+                    >
+                        <div
+                            className=" grid grid-cols-5"
+                            style={{ height: "100%" }}
+                        >
+                            <div className="login__cover-photo col-span-3 rounded-tl-lg rounded-bl-lg h-100%"></div>
+                            <div
+                                className="col-span-2 rounded-tr-lg rounded-br-lg h-100% text-center"
+                                style={{
+                                    backgroundColor: "#FBFFDC",
+                                }}
+                            >
+                                <div className="p-5">
+                                    <h3
+                                        className="text-center my-3 title"
+                                        style={{ color: "#F24C3D" }}
+                                    >
+                                        Login Page
+                                    </h3>
+                                    <StyledFirebaseAuth
+                                        uiConfig={uiConfig}
+                                        firebaseAuth={auth}
+                                    />
+                                    <p>
+                                        You care about your money?
+                                        <br />
+                                        We care about you
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className=" flex justify-center items-center">
-          <div
-            className="login rounded-lg"
-            style={{ height: "600px", width: "1000px" }}
-          >
-            <div className=" grid grid-cols-5" style={{ height: "100%" }}>
-              <div className="col-span-3 bg-red-400 rounded-tl-lg rounded-bl-lg   h-100%"></div>
-              <div className="col-span-2 bg-yellow-100 rounded-tr-lg rounded-br-lg h-100% text-center">
-                <div className="p-5">
-                  <h1 className="text-center my-3 title">My App</h1>
-                  <p>
-                    Welcome {auth.currentUser.displayName}! You are now
-                    signed-in!
-                  </p>
-                  <button onClick={() => auth.signOut()}>Sign-out</button>
+            ) : (
+                <div className=" flex justify-center items-center">
+                    <div
+                        className="login rounded-lg"
+                        style={{ height: "600px", width: "1000px" }}
+                    >
+                        <div
+                            className=" grid grid-cols-5"
+                            style={{ height: "100%" }}
+                        >
+                            <div className="col-span-3 bg-red-400 rounded-tl-lg rounded-bl-lg   h-100%"></div>
+                            <div className="col-span-2 bg-yellow-100 rounded-tr-lg rounded-br-lg h-100% text-center">
+                                <div className="p-5">
+                                    <h1 className="text-center my-3 title">
+                                        My App
+                                    </h1>
+                                    <p>
+                                        Welcome {auth.currentUser.displayName}!
+                                        You are now signed-in!
+                                    </p>
+                                    <button onClick={() => auth.signOut()}>
+                                        Sign-out
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+            )}
+        </>
+    );
 };
 
 export default LoginComponent;
